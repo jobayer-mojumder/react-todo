30f0b2a862964870179718150339121f5ce205bc
jobayermojumder <jobayer.pro@gmail.com>
2020-07-12 19:20:14 +0200

    first commit

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <footer>
+            <h4>this is footer</h4>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/components/header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <nav>
+            <h1>Logo</h1>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Header;
