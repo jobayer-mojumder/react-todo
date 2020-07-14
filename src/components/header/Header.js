3c4aafca5cca2fcfb1e6af9eef491cd4abcca752
jobayermojumder <jobayer.pro@gmail.com>
2020-07-14 22:49:34 +0200

    nav bar modified

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,9 +6,7 @@ function Header() {
         <div className="header">
             <a href="#default" className="logo">CompanyLogo</a>
             <div className="header-right">
-                <a className="active" href="#home">Home</a>
-                <a href="#contact">Contact</a>
-                <a href="#about">About</a>
+                <a href="#home">Home</a>
             </div>
         </div>
     );
